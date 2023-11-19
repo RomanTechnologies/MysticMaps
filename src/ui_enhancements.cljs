0b8367af440f55d03d5f15e8f953517c999d832b
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2023-11-19 23:29:35 +0000

    Enhance UI with advanced interactive features

--- src/ui_enhancements.cljs
+++ src/ui_enhancements.cljs
@@ -1,4 +1,10 @@
 (ns mystic-maps.ui.enhancements
 
 (defn enhanced-map-view [data]
-  [h:div \"This is the enhanced map view component, with additional interactive elements.\"))"}
+  (let [elements ([{:name \"Tree\" :width 500} {:type \"Mountain\" :height 300}])]
+    [h:div
+
+      [h:ul for [el in elements]
+        [h:li {:key i }:name el:name]]
+      )]
+    )
