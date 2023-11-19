92296e2ea9a1b12d37f022e863cfd8bc351259cb
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2023-11-19 23:33:16 +0000

    Enhance main application logic

--- src/main.clj
+++ src/main.clj
@@ -1,4 +1,10 @@
 (ns mystic-maps.main
 
+(require ['mystic-maps.ui' 'mongodb-client'])
+(def db-connection (mongodb-client/get-db "MysticMaps"))
+(def get-map (fn [map-id] (mongodb-client/find-one db-connection \"Maps\" {:_id map-id})))
+
 (def main ([args]
-  (puts "Welcome to Mystic Maps"))
+  (let [maps (map get-map [1, 2, 3])]
+    (for [map in maps]
+      (puts (json-string map))))
